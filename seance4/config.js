25f926e22a92a15399abe79d58128bc18154581a
tomfevrier <tomfevrier@gmail.com>
2019-10-01 21:03:13 +0200

    Séance 3

--- seance4/config.js
+++ seance4/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'zO8nZ4oLxT7VAeMNONhguEBUu',
-    consumer_secret: '7u0TSayz5ZbcoIAiaOGUsCOHdqsxCLW8SmXGy2An44T5UpU8B0',
-    access_token: '1175672495527157760-vrm0zhz3IYmZWWPHvVXsUDVWFb9ScU',
-    access_token_secret: 'zBEos0MSsab7F6SOujchMrrYg72c6oeqVsz6XmzLWZ484',
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
 }
