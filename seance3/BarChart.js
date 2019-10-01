75f1ff36fb622044a056215435ba190cba0356d4
tomfevrier <tomfevrier@gmail.com>
2019-10-01 21:01:39 +0200

    Séance 3

--- seance3/BarChart.js
+++ seance3/BarChart.js
@@ -8,7 +8,34 @@ export default class BarChart {
 
 	draw() {
 
-		// Nesting goes here
+		/*
+		La méthode d3.nest() permet de grouper les données en fonction d'une clé.
+		Elle se décomposent en deux méthodes :
+		- key() prend en paramètre une fonction accesseur qui renvoie la
+		propriété qui sert de clé
+		- entries() prend en paramètre les données
+		A cela peut se rajouter la méthode rollup(), qui précise quelle valeur
+		associer à chaque clé
+		*/
+		this.nest = d3.nest()
+			.key(function(d) {
+				return d.genre;
+			})
+			.rollup(function(v) {
+				/*
+				Au lieu de renvoyer le tableau contenant tous les films d'un
+				genre donné, on ne renvoie que le nombre de films
+				*/
+				return v.length;
+			})
+			.entries(this.data);
+
+		// On trie le tableau par ordre décroissant du nombre de films
+		this.nest = this.nest.sort(function(a, b) {
+			return b.value - a.value;
+		});
+
+		console.log(this.nest);
 
 		this.height = 600;
 		this.width = 1000;
@@ -30,9 +57,23 @@ export default class BarChart {
 
 	createScales() {
 
-		this.xScale = null;
+		// Voir la doc : https://github.com/d3/d3-scale
+		this.xScale = d3.scaleBand()
+			// La méthode map() renvoie ici la liste des clés de this.nest
+			.domain(this.nest.map(function(e) {
+				return e.key;
+			}))
+			.rangeRound([this.margin, this.width - this.margin])
+			/*
+			paddingInner() et paddingOuter() configurent l'espacement entre les
+			barres et autour des barres respectivement
+			*/
+			.paddingInner(0.1)
+			.paddingOuter(0.1);
 
-		this.yScale = null;
+		this.yScale = d3.scaleLinear()
+			.domain([0, 8])
+			.rangeRound([this.height - this.margin, this.margin]);
 
 	}
 
@@ -62,8 +103,30 @@ export default class BarChart {
 
 		this.bars = this.svg.append('g');
 
-		// Code goes here
-	
+		this.bars.selectAll('rect')
+			.data(this.nest).enter()
+			.append('rect')
+				.attr('x', function(d) {
+					return that.xScale(d.key);
+				})
+				.attr('y', this.yScale(0))
+				.attr('width', this.xScale.bandwidth())
+				.attr('height', 0)
+				.attr('fill', 'purple')
+				.transition()
+				.duration(2000)
+					/*
+					Pour animer les barres, il faut animer à la fois la hauteur
+					des rectangles (de 0 jusqu'à la hauteur voulue) mais aussi
+					sa position y (du bas de l'échelle jusqu'à la valeur voulue)
+					*/
+					.attr('y', function(d) {
+						return that.yScale(d.value);
+					})
+					.attr('height', function(d) {
+						return that.yScale(0) - that.yScale(d.value);
+					})
+
 	}
 
 }
