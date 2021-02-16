be8517b12f27fdae7f8542a9b5d57490c85e694a
Tom Février <tomfevrier@gmail.com>
2021-02-16 21:38:41 +0100

    Mise à jour séance 2

--- seance2/index.js
+++ seance2/index.js
@@ -17,29 +17,29 @@ const getMovies = async () => {
 	const movies = [];
 
 	$('.lister-item-content').each((index, element) => {
-        /*
-        On extrait les informations dont on a besoin à l'aide de sélecteurs (classes, balises, attributs, etc.) :
-        - la méthode find() renvoie le ou les éléments correspondant au sélecteur fourni dans la sélection actuelle
-        - la méthode text() renvoie le texte de l'élément sélectionné (sans les balises HTML)
-        - la méthode attr() renvoie la valeur de l'attribut fourni dans l'élément sélectionné
-        Voir la documentation de Cheerio pour plus d'informations : https://cheerio.js.org/
+		/*
+		On extrait les informations dont on a besoin à l'aide de sélecteurs (classes, balises, attributs, etc.) :
+		- la méthode find() renvoie le ou les éléments correspondant au sélecteur fourni dans la sélection actuelle
+		- la méthode text() renvoie le texte de l'élément sélectionné (sans les balises HTML)
+		- la méthode attr() renvoie la valeur de l'attribut fourni dans l'élément sélectionné
+		Voir la documentation de Cheerio pour plus d'informations : https://cheerio.js.org/
 
 		On nettoie les données avec la méthode replace(), qui permet de remplacer un morceau d'une chaîne par un autre.
-        Elle prend deux paramètres :
-        - la chaîne à remplacer, sous forme d'une chaîne de caractères ou d'une expression régulière (RegEx)
-        - la chaîne par laquelle remplacer (pour supprimer, une chaîne vide "")
+		Elle prend deux paramètres :
+		- la chaîne à remplacer, sous forme d'une chaîne de caractères ou d'une expression régulière (RegEx)
+		- la chaîne par laquelle remplacer (pour supprimer, une chaîne vide "")
 
 		Le "+" devant une variable permet de convertir la valeur d'une chaîne de caractères vers un nombre
-        */
+		*/
 
-        const title = $(element).find('.lister-item-header a').text();
-        const runtime = +$(element).find('.runtime').text().replace(" min", "");
-        const rating = +$(element).find('.ratings-imdb-rating').attr('data-value');
-        const boxOffice = +$(element).find('.sort-num_votes-visible span').last().attr('data-value').replace(/,/g, "");
+		const title = $(element).find('.lister-item-header a').text();
+		const runtime = +$(element).find('.runtime').text().replace(" min", "");
+		const rating = +$(element).find('.ratings-imdb-rating').attr('data-value');
+		const boxOffice = +$(element).find('.sort-num_votes-visible span').last().attr('data-value').replace(/,/g, "");
 
-        // On ajoute nos données dans notre tableau movies
-        movies.push({ title, runtime, rating, boxOffice });
-    });
+		// On ajoute nos données dans notre tableau movies
+		movies.push({ title, runtime, rating, boxOffice });
+	});
 
 	// On exporte notre tableau dans un fichier JSON
 	fs.writeFileSync('movies.json', JSON.stringify(movies, null, '\t'));
