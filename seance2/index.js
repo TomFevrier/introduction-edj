be4fdb1fdd59dc09f576e551ffada1470445d08a
Tom Février <tomfevrier@gmail.com>
2021-02-16 21:37:37 +0100

    Mise à jour séance 2

--- seance2/index.js
+++ seance2/index.js
@@ -1,57 +1,54 @@
-// On importe la bibliothèque fs ("file system")
+// On importe la bibliothèque fs ("file system", pour écrire des fichiers)
 const fs = require('fs');
-// On importe la bibliothèque request-promise (qui permet de récupérer le contenu d'une page web)
-const rp = require('request-promise');
+// On importe la bibliothèque node-fetch (qui permet de récupérer le contenu d'une page web)
+const fetch = require('node-fetch');
 // On importe la bibliothèque cheerio (qui permet d'extraire le contenu qui nous intéresse)
 const cheerio = require('cheerio');
 
-// On crée un fichier CSV avec le header (noms des colonnes)
-fs.writeFileSync("data.csv", "title,runtime,rating,boxOffice\n");
 
-/*
-On définit les options pour que request-promise retourne le contenu de la page web :
-- uri : l'URL de la page que l'on veut scraper
-- transform : comment le contenu de la page doit être utilisé (ici, il doit être chargé dans Cheerio)
-*/
-const options = {
-    uri: "https://www.imdb.com/search/title?title_type=feature&year=2018-01-01,2018-12-31&sort=boxoffice_gross_us,desc",
-    transform: function(body) {
-        return cheerio.load(body);
-    }
-};
+const getMovies = async () => {
+	// On fait une requête vers notre page pour récupérer son contenu
+	const response = await fetch('https://www.imdb.com/search/title/?year=2019&title_type=feature&sort=boxoffice_gross_us,desc');
+	// On récupère le résultat de notre requête sous forme de texte
+	const html = await response.text();
+	// On charge le HTML dans Cheerio pour le manipuler
+	const $ = cheerio.load(html);
 
-rp(options)
-    .then(function($) {
-        $('.lister-item-content').each(function(index, element) {
-            /*
-            On extrait les informations dont on a besoin à l'aide de sélecteurs (classes, balises, attributs, etc.) :
-            - la méthode find() renvoie le ou les éléments correspondant au sélecteur fourni dans la sélection actuelle
-            - la méthode text() renvoie le texte de l'élément sélectionné (sans les balises HTML)
-            - la méthode attr() renvoie la valeur de l'attribut fourni dans l'élément sélectionné
-            Voir la documentation de Cheerio pour plus d'informations : https://cheerio.js.org/
-            */
-            var title = $(element).find('.lister-item-header a').text();
-            var runtime = $(element).find('.runtime').text();
-            var rating = $(element).find('.ratings-imdb-rating').attr('data-value');
-            var boxOffice = $(element).find('.sort-num_votes-visible span').last().attr('data-value');
+	const movies = [];
 
-            /*
-            On nettoie les données avec la méthode replace(), qui permet de remplacer un morceau d'une chaîne par un autre.
-            Elle prend deux paramètres :
-            - la chaîne à remplacer, sous forme d'une chaîne de caractère ou d'une expression régulière (RegEx)
-            - la chaîne par laquelle remplacer (pour supprimer, une chaîne vide "")
-            */
-            runtime = runtime.replace(" min", "");
-            boxOffice = boxOffice.replace(/,/g, ""); // le g (global) indique que l'on souhaite supprimer toutes les virgules, pas simplement la première
+	$('.lister-item-content').each((index, element) => {
+        /*
+        On extrait les informations dont on a besoin à l'aide de sélecteurs (classes, balises, attributs, etc.) :
+        - la méthode find() renvoie le ou les éléments correspondant au sélecteur fourni dans la sélection actuelle
+        - la méthode text() renvoie le texte de l'élément sélectionné (sans les balises HTML)
+        - la méthode attr() renvoie la valeur de l'attribut fourni dans l'élément sélectionné
+        Voir la documentation de Cheerio pour plus d'informations : https://cheerio.js.org/
 
-            // On écrit la ligne dans notre fichier CSV
-            fs.appendFileSync("data.csv", `${title},${runtime},${rating},${boxOffice}\n`);
-            // "Template strings" : le `texte` est entre backticks et les ${variables} insérées avec un signe $ et entre accolades
-        })
+		On nettoie les données avec la méthode replace(), qui permet de remplacer un morceau d'une chaîne par un autre.
+        Elle prend deux paramètres :
+        - la chaîne à remplacer, sous forme d'une chaîne de caractères ou d'une expression régulière (RegEx)
+        - la chaîne par laquelle remplacer (pour supprimer, une chaîne vide "")
+
+		Le "+" devant une variable permet de convertir la valeur d'une chaîne de caractères vers un nombre
+        */
+
+        const title = $(element).find('.lister-item-header a').text();
+        const runtime = +$(element).find('.runtime').text().replace(" min", "");
+        const rating = +$(element).find('.ratings-imdb-rating').attr('data-value');
+        const boxOffice = +$(element).find('.sort-num_votes-visible span').last().attr('data-value').replace(/,/g, "");
+
+        // On ajoute nos données dans notre tableau movies
+        movies.push({ title, runtime, rating, boxOffice });
     });
+
+	// On exporte notre tableau dans un fichier JSON
+	fs.writeFileSync('movies.json', JSON.stringify(movies, null, '\t'));
+}
+
+getMovies();
 
 /*
 A toi de jouer ! Essaye de...
 - ne récupérer que les dix plus gros succès de l'année (indice : utilise le paramètre "index")
-- récupérer les plus gros succès de toutes les années entre 1997 et 2018 (indice : regarde comment est construite l'URL et utilise une boucle for)
+- récupérer les plus gros succès de toutes les années entre 1997 et 2019 (indice : regarde comment est construite l'URL et utilise une boucle for)
 */
